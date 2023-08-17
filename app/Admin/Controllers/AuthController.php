c3897f6fa99e69ea125e97d3494ccd0081f0a810
johnson <johnson27945@outlook.com>
2023-08-17 12:40:41 +1200

    后台添加默认页面

--- app/Admin/Controllers/AuthController.php
+++ app/Admin/Controllers/AuthController.php
@@ -3,9 +3,18 @@
 namespace App\Admin\Controllers;
 
 use Encore\Admin\Controllers\AuthController as BaseAuthController;
+use Illuminate\Http\Request;
 
 class AuthController extends BaseAuthController
 {
     public $redirectTo="/admin/teachers";
 
+    public function getLogout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        return redirect("/admin/auth/login");
+    }
 }
